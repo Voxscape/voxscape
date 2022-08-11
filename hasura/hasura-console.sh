5f939403e9b40a57c53d5a08ee2b95fd2d1311cc
Wang Guan <momocraft@gmail.com>
2022-08-11 19:32:34 +0900

    create user table

--- hasura/hasura-console.sh
+++ hasura/hasura-console.sh
@@ -3,8 +3,8 @@
 set -ue
 cd "$(dirname "$0")"
 
-export HASURA_GRAPHQL_ENDPOINT='http://127.0.0.1:61080'
+export HASURA_GRAPHQL_ENDPOINT='http://127.0.0.1:62080'
 export HASURA_GRAPHQL_ADMIN_SECRET='super_secret_admin_secret'
 
 set -x
-exec npm run hasura "$@"
+exec npm run hasura console "$@"
