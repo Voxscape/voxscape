5379340b42e316297a7376be9d99a45ca84c76d2
Wang Guan <momocraft@gmail.com>
2022-08-22 02:04:15 +0900

    unwrap quill from cats.effect.Resource

--- server/api-server/src/main/scala/io/jokester/nuthatch/infra/ApiContext.scala
+++ server/api-server/src/main/scala/io/jokester/nuthatch/infra/ApiContext.scala
@@ -4,8 +4,6 @@ import cats.effect.IO
 import cats.effect.kernel.Resource
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
-import io.getquill.PostgresJdbcContext
-import io.jokester.quill.QuillCirceJsonEncoding
 import redis.clients.jedis.Jedis
 
 import scala.util.{Failure, Try}
@@ -14,12 +12,14 @@ class ApiContext(val rootConfig: Config) extends LazyLogging {
   private val jedisPool = RedisFactory.poolFromConfig(rootConfig.getConfig("redis.default"))
   def redis: Resource[IO, Jedis] = RedisFactory.wrapJedisPool(jedisPool)
 
-  def quill: Resource[IO, QuillFactory.PublicCtx] =
-    QuillFactory.createNonPooledCtx(rootConfig.getConfig("database.default"))
+  private val quillResources = QuillFactory.unsafeCreatePooledQuill(rootConfig.getConfig("database.default"))
+
+  val quill: QuillFactory.PublicCtx = quillResources._2
 
   def close(): IO[Unit] = IO.blocking {
     Try {
       logger.debug("closing Jedis pool")
+      quillResources._1.close()
       jedisPool.close()
     } match {
       case Failure(exception) => logger.warn("error closing Jedis pool", exception)
--- server/api-server/src/main/scala/io/jokester/nuthatch/infra/QuillFactory.scala
+++ server/api-server/src/main/scala/io/jokester/nuthatch/infra/QuillFactory.scala
@@ -5,7 +5,7 @@ import cats.effect.IO
 import cats.effect.kernel.Resource
 import com.typesafe.config.Config
 import com.zaxxer.hikari.HikariDataSource
-import io.getquill.{PostgresDialect, PostgresJdbcContext}
+import io.getquill.{PostgresDialect, PostgresJdbcContext, SnakeCase}
 import io.jokester.nuthatch.quill.generated.public.PublicExtensions
 import io.jokester.quill.{QuillCirceJsonEncoding, QuillDataSource, QuillDatetimeEncoding}
 import io.jokester.quill.QuillDataSource.FixedPostgresNaming
@@ -13,24 +13,24 @@ import io.jokester.quill.QuillDataSource.FixedPostgresNaming
 import javax.sql.DataSource
 
 object QuillFactory {
-  class PublicCtx(dataSource: DataSource with Closeable) extends PostgresJdbcContext[FixedPostgresNaming.type](FixedPostgresNaming, dataSource)
-    with PublicExtensions[PostgresDialect, FixedPostgresNaming.type]
-    with QuillCirceJsonEncoding
-    with QuillDatetimeEncoding
+  class PublicCtx(dataSource: DataSource with Closeable)
+      extends PostgresJdbcContext(SnakeCase, dataSource)
+      with PublicExtensions[PostgresDialect, SnakeCase.type]
+      with QuillCirceJsonEncoding
+      with QuillDatetimeEncoding
 
   def createNonPooledCtx(c: Config): Resource[IO, PublicCtx] = {
     val simpleDataSource = QuillDataSource.simplePgDataSource(c.getString("url"))
     Resource.make(
-      IO.blocking(new PublicCtx(simpleDataSource))
+      IO.blocking(new PublicCtx(simpleDataSource)),
     )(ctx => IO.blocking({ ctx.close() }))
   }
 
-  def createPooledQuillContext(c: Config): IO[(HikariDataSource, PublicCtx)] = IO {
+  def unsafeCreatePooledQuill(c: Config): (HikariDataSource, PublicCtx) = {
     val simpleDataSource = QuillDataSource.simplePgDataSource(c.getString("url"))
     val hikariDataSource = QuillDataSource.pooled(simpleDataSource)
 
     val ctx = new PublicCtx(hikariDataSource)
-
     (hikariDataSource, ctx)
   }
 }
