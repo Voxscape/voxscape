d317deb9817c3840b2eb52bfee8dcde4696b4629
Wang Guan <momocraft@gmail.com>
2022-08-29 03:04:08 +0900

    wip

--- server/api-server/src/main/scala/io/jokester/nuthatch/scopes/authn/AuthenticationService.scala
+++ server/api-server/src/main/scala/io/jokester/nuthatch/scopes/authn/AuthenticationService.scala
@@ -3,6 +3,6 @@ package io.jokester.nuthatch.scopes.authn
 import com.typesafe.scalalogging.LazyLogging
 import io.jokester.nuthatch.infra.ApiContext
 
-trait AuthenticationService extends TwitterOAuth1 with LazyLogging {
+trait AuthenticationService extends TwitterOAuth1 with OAuthTokenProvider with LazyLogging {
   protected val apiCtx: ApiContext
 }
---
+++ server/api-server/src/main/scala/io/jokester/nuthatch/scopes/twitter/UserTwitterStream.scala
@@ -0,0 +1,8 @@
+package io.jokester.nuthatch.scopes.twitter
+
+import io.jokester.nuthatch.scopes.authn.OAuthTokenProvider
+
+object UserTwitterStream {
+
+  def find(provider: OAuthTokenProvider): Unit = {}
+}
