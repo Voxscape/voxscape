5378ef1676cd405a1321785778cc9f4eea4474bd
Wang Guan <momocraft@gmail.com>
2022-11-03 02:48:23 +0900

    tweak light / camera

--- web/src/components/mrk-system/scene-builders.ts
+++ web/src/components/mrk-system/scene-builders.ts
@@ -5,33 +5,42 @@ import { Color3, Color4, Vector3 } from '@babylonjs/core';
 
 export const createShirtPreviewScene = (modelFile: File): SceneBuilder => {
   return async (sceneLoader, engine) => {
-    const scene = await sceneLoader.LoadAsync('/blender/', 'shirt-split3.gltf', engine, (ev) =>
+    const scene = await sceneLoader.LoadAsync('/blender/', 'shirt-split5.gltf', engine, (ev) =>
       console.debug('progress', ev),
     );
     console.debug(__filename, 'loaded', scene);
     resetColor(scene);
+    // resetLight(scene);
     // activeDefaultCamera(scene);
     activeArcCamera(scene);
+
     return scene;
   };
 };
 
 export function resetColor(scene: Scene): void {
   scene.clearColor = Color3.Black().toColor4(0.9);
+  scene.ambientColor = Color3.White();
+}
+
+export function resetLight(scene: Scene): void {
+  scene.lights.forEach((l) => l.dispose());
+}
+
+function resetAmbientColor(scene: Scene): void {
+  scene.meshes.forEach((mesh) => {});
 }
 
 export function activeArcCamera(scene: Scene): void {
   const camera0 = scene.cameras[0];
+  camera0.dispose();
   const arcCam = createArcRotateCamera(scene);
-  const dist = camera0.position.length();
   arcCam.position = camera0.position;
   arcCam.target = Vector3.Zero();
+  arcCam.lowerRadiusLimit = 2;
   arcCam.radius = 2;
-  // arcCam.lowerRadiusLimit = dist * 3;
-  // arcCam.upperRadiusLimit = dist * 5;
-  // scene.removeCamera(camera0);
+  arcCam.upperRadiusLimit = 6;
   scene.setActiveCameraById(arcCam.id);
-  camera0.dispose();
   console.debug('cameras cleared', scene.cameras);
 }
 
