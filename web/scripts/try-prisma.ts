0c89bdeb4457fb90faefa92c2f81d029157a946b
Wang Guan <momocraft@gmail.com>
2023-02-06 02:38:31 +0900

    wip

---
+++ web/scripts/try-prisma.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const allUsers = await prisma.user.findMany({where: {}});
+  console.debug('allUsers', allUsers);
+  const user = await prisma.user.create({
+    data: {
+      name: 'alize',
+      email: 'alice@prisma.io',
+    },
+  });
+  // ... you will write your Prisma Client queries here
+  console.debug('user', user);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+
+  .catch(async (e) => {
+    console.error(e);
+
+    await prisma.$disconnect();
+
+    process.exit(1);
+  });
