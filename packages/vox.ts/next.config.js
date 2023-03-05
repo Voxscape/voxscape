57ea0c0b42b03635d1214ccf803072acb0f53ae7
Wang Guan <momocraft@gmail.com>
2023-03-05 18:50:05 +0900

    fix nextjs config

--- packages/vox.ts/next.config.js
+++ packages/vox.ts/next.config.js
@@ -7,24 +7,6 @@ const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/const
 const nextConf = {
   poweredByHeader: false,
 
-  // see https://nextjs.org/docs/#customizing-webpack-config
-  webpack(config, { buildId, dev, isServer, webpack }) {
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        /**
-         * build-time configuration
-         */
-        'process.env.NEXT_DEV': JSON.stringify(!!dev),
-      }),
-    );
-
-    config.node = {
-      // allow use of __file / __dirname
-      ...config.node,
-      __filename: true,
-    };
-    return config;
-  },
   reactStrictMode: true,
 };
 
@@ -37,8 +19,6 @@ module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_PRODUCTION_BUILD) {
     merged = require('@next/bundle-analyzer')({ enabled: true, openAnalyzer: false })(merged);
   }
-
-  merged = require('next-transpile-modules')(['lodash-es', '@jokester/ts-commonutil'])(merged);
 
   return merged;
 };
