b107bec09cfce1563b42d82c128955f56a6247fa
Wang Guan <momocraft@gmail.com>
2023-03-05 18:44:06 +0900

    ref-models: save as ndjson

--- packages/ref-models/index.sh
+++ packages/ref-models/index.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd $(dirname "$0")
 
-find -name '*.vox' | xargs ../vox.ts/bin/voxprobe > index.txt 2> index.err
+find -name '*.vox' | xargs ../vox.ts/bin/voxprobe > index.ndjson 2> index.err
