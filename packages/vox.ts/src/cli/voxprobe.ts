456e9e6341c80a0c4148ff902044fced9922e48e
Wang Guan <momocraft@gmail.com>
2023-03-05 18:22:00 +0900

    voxprobe: print nd-json

--- packages/vox.ts/src/cli/voxprobe.ts
+++ packages/vox.ts/src/cli/voxprobe.ts
@@ -1,9 +1,6 @@
 import fsp from 'node:fs/promises';
 import { basicParser } from '../parser/basic-parser';
 
-function printTsv(...columns: (string | number)[]) {
-  console.log(columns.join('\t'));
-}
 async function main(...args: string[]): Promise<void> {
   if (!args.length) {
     console.log(`Usage: voxprobe <vox-file>`);
@@ -15,21 +12,16 @@ async function main(...args: string[]): Promise<void> {
     const bytes = await fsp.readFile(arg);
     try {
       const parsed = basicParser(bytes.buffer);
-      printTsv('FILE', arg, bytes.byteLength, parsed.models.length, parsed.materials.length);
-      printTsv(
-        'JSON',
-        JSON.stringify({
-          path: arg,
-          modelCount: parsed.models.length,
-          modelMeta: parsed.models.map((m) => ({ ...m.size, numVoxel: m.voxels.length })),
-        }),
-      );
-      for (const [modelId, model] of parsed.models.entries()) {
-        printTsv('MODEL', modelId, model.voxels.length, model.size.x, model.size.y, model.size.z);
-      }
+      const f = {
+        path: arg,
+        models: parsed.models.map((m) => ({
+          size: m.size,
+          numVoxels: m.voxels.length,
+        })),
+      };
+      console.log(JSON.stringify(f));
     } catch (e: any) {
       console.error(arg, e);
-      printTsv('ERROR', arg, e?.message);
     }
   }
 }
