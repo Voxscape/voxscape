413d506dc8b560c51f9437e7157467ca2f5fcf50
Wang Guan <momocraft@gmail.com>
2023-03-29 23:21:27 +0900

    wip

---
+++ web/server/external/gcp.ts
@@ -0,0 +1,21 @@
+import { Bucket, Storage } from '@google-cloud/storage';
+
+let storage: Storage;
+
+function getStorage(): Storage {
+  if (!storage) {
+    if (process.env.GOOGLE_SERVICE_ACCOUNT_CRED) {
+      storage ||= new Storage({
+        credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_CRED),
+      });
+    } else {
+      // we'd better be running in GCE
+      storage ||= new Storage();
+    }
+  }
+  return storage;
+}
+
+export function getBucket(name = process.env.GOOGLE_STORAGE_BUCKET!): Bucket {
+  return getStorage().bucket(name);
+}
