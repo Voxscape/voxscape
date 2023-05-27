624b63263f002912fba8d059cb80007a1ef6051c
Wang Guan <momocraft@gmail.com>
2023-05-27 13:59:27 +0900

    revert missing next config

--- packages/vox.ts/next.config.js
+++ packages/vox.ts/next.config.js
@@ -8,6 +8,39 @@ const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/const
 const nextConf = {
   poweredByHeader: false,
 
+  /**
+   * runtime server-only configuration
+   * @type {import('./server/runtime-config').ServerRuntimeConfig}
+   */
+  serverRuntimeConfig: {
+    serverStartAt: new Date().toISOString(),
+  },
+  /**
+   * build-time configuration
+   */
+  env: {
+    // becomes process.env.SOME_CONSTANT : boolean
+    builtAt: new Date().toISOString(),
+  },
+  // see https://nextjs.org/docs/#customizing-webpack-config
+  webpack(config, { buildId, dev, isServer, webpack }) {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        /**
+         * build-time configuration
+         */
+        'process.env.NEXT_DEV': JSON.stringify(!!dev),
+      }),
+    );
+
+    config.node = {
+      // allow use of __file / __dirname
+      ...config.node,
+      __filename: true,
+    };
+    return config;
+  },
+
   transpilePackages: ['lodash-es', '@jokester/ts-commonutil'],
 
   reactStrictMode: true,
