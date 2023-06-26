2414e05fe45609e5854de358b017a1e0dedae63d
Wang Guan <momocraft@gmail.com>
2023-06-27 00:52:12 +0900

    wip

--- web/server/prisma.spec.ts
+++ web/server/prisma.spec.ts
@@ -1,4 +1,5 @@
 import { prisma } from './prisma';
+import {faker} from '@faker-js/faker'
 
 describe('prisma', () => {
   it('connects to db', async () => {
@@ -7,3 +8,34 @@ describe('prisma', () => {
     expect(q).toEqual([{ result: 2 }]);
   });
 });
+
+describe('prisma with generated model classes', () => {
+  it('CRUD on User', async () => {
+
+    const user = await prisma.user.create({
+      data: {
+        email: faker.internet.email(),
+      }
+    })
+
+    expect(user.email).toMatch(/.+@.+\..+/)
+    expect(user.emailVerified).toBe(null)
+
+    /**
+     * email is in small case
+     */
+    const notFound = await prisma.user.findUnique({
+      where: {
+        email: user.email?.toUpperCase(),
+      }
+    })
+    expect(notFound).toEqual(null)
+
+    const reloaded = await prisma.user.findUnique({
+      where: {
+        email: user.email!,
+      }
+    })
+    expect(reloaded).toBeTruthy()
+  })
+})
