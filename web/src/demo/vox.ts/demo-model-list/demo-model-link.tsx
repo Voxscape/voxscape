b0d2916ab6de3dc5ff3db280d2a9163de670c590
Wang Guan <momocraft@gmail.com>
2023-09-11 05:02:29 +0900

    update

--- web/src/demo/vox.ts/demo-model-list/demo-model-link.tsx
+++ web/src/demo/vox.ts/demo-model-list/demo-model-link.tsx
@@ -1,13 +1,18 @@
 import Link from 'next/link';
 import { RefModelIndexEntry } from './ref-models';
 import { ReactElement } from 'react';
+import { Button } from '@chakra-ui/react';
 
-export function DemoModelLink({ entry, index }: { entry: RefModelIndexEntry; index: number }): ReactElement {
+export function DemoModelLink({
+  entry,
+  index,
+  children,
+}: React.PropsWithChildren<{ entry: RefModelIndexEntry; index: number }>): ReactElement {
   const href = `/demo/vox.ts/show?file=${encodeURIComponent(entry.path.replaceAll(/^\.\//g, ''))}&modelIndex=${index}`;
 
   return (
     <Link href={href} target="_blank">
-      Open
+      <Button>{children}</Button>
     </Link>
   );
 }
