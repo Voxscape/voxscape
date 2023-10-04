392dafe1933647606cdf8cb561a4c49787745d6e
Wang Guan <momocraft@gmail.com>
2023-10-05 03:33:50 +0900

    make linter happy

--- web/pages/dev/components.tsx
+++ web/pages/dev/components.tsx
@@ -4,19 +4,17 @@ import { LayoutHeader } from '../../src/layout/header';
 import { pixelBorders } from '../../src/styles/pixelBorders';
 import { createDebugLogger } from '../../shared/logger';
 
-const logger = createDebugLogger(__filename)
+const logger = createDebugLogger(__filename);
 
 const ComponentsDemoPage = () => {
-
-  logger('pixelBorders', pixelBorders)
+  logger('pixelBorders', pixelBorders);
 
   return (
     <div>
       <LayoutHeader />
       <hr className="my-2" />
-      <div className='container'>
+      <div className="container">
         <div className={clsx(pixelBorders.box.light)}>box-light</div>
-
       </div>
       <hr className="my-2" />
       <ChakraDemo />
