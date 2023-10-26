0d2ded9fd8571c6eb6d36c42bccd5d823b51fb06
Wang Guan <momocraft@gmail.com>
2023-10-27 01:14:02 +0900

    show pwa install button

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,6 +1,12 @@
 import { NextPage } from 'next';
 import { Layout } from '../src/layout/layout';
+import '@pwabuilder/pwainstall';
 const IndexPage: NextPage = (props) => {
-  return <Layout>TODO</Layout>;
+  return (
+    <Layout>
+      {/* @ts-ignore */}
+      <pwa-install></pwa-install>
+    </Layout>
+  );
 };
 export default IndexPage;
