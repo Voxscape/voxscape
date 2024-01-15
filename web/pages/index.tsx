cdc70e94640c3453ed244ae96121e5df22589a4f
Wang Guan <momocraft@gmail.com>
2024-01-16 02:08:28 +0900

    make lint happy & run typecheck first

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from 'next';
 import { Layout } from '../src/layout/layout';
-import { IndexPageModelList } from "../src/model/list/model-list";
+import { IndexPageModelList } from '../src/model/list/model-list';
 const IndexPage: NextPage = (props) => {
   return (
     <Layout>
